8e4da9c57fb0b3a6fd7c991ed54a901d066e8bed
maxim-rostagno <61866902+maxim-rostagno@users.noreply.github.com>
2021-02-09 22:41:18 +0100

    11
    Test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import firebase from 'firebase';
+// core components
+import Admin from "layouts/Admin.js";
+import RTL from "layouts/RTL.js";
+import Login from "layouts/Login.js";
+
+import "assets/css/material-dashboard-react.css?v=1.9.0";
+
+const hist = createBrowserHistory();
+const firebaseConfig = {
+  apiKey: "AIzaSyBKS6ecNzchqEYq2LJ0VdlBHJakXNSNSTI",
+  authDomain: "webdesk-74f51.firebaseapp.com",
+  databaseURL: "https://webdesk-74f51-default-rtdb.firebaseio.com",
+  projectId: "webdesk-74f51",
+  storageBucket: "webdesk-74f51.appspot.com",
+  messagingSenderId: "896441490259",
+  appId: "1:896441490259:web:d6b0ca1b3dfde9b1dbdfa6",
+  measurementId: "G-GN1BCFYQTQ"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/rtl" component={RTL} />
+      <Route path="/login" component={Login} />
+    <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
